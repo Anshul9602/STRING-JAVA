5cd6b8610ade8a1ca54fde415a3bf91991147739
Anshul <81064196+Anshul9602@users.noreply.github.com>
2022-04-06 11:07:01 +0530

    Add files via upload

---
+++ inputstring.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class inputstring {
+    public static void main(String[] args){
+       /* String name = new String("anshul");
+        System.out.println(name);
+
+        // print formate
+
+        System.out.print("ansu");
+        int a =5;
+        float b = 1.2f ;
+        System.out.printf("the value of a is %d and the value of b is %f" ,a,b);
+*/
+        // string input
+
+        Scanner sc = new Scanner(System.in);
+        String st =sc.next();
+        System.out.println(st);
+    }
+}
